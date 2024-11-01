50624b1d019c12a77ba986e6941e3926f7adcbc2
oscarpoblacion@gmail.com <oscarpoblacion@gmail.com>
2024-11-01 19:02:47 +0000

    Created ServerModule1 and Form1.ItemTemplate1; Edited settings and Form1

---
+++ server_code/ServerModule1.py
@@ -0,0 +1,42 @@
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.email
+import anvil.server
+from datetime import datetime
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+@anvil.server.callable
+def send_feedback(name, email, feedback):
+  # Send yourself an email each time feedback is submitted
+  anvil.email.send(to="oscarpoblacion@gmail.com", # Change this to your email address!
+                   subject=f"Feedback from {name}",
+                   text=f"""
+                   
+  A new person has filled out the feedback form!
+
+  Name: {name}
+  Email address: {email}
+  Feedback:
+  {feedback}
+  """)
+  app_tables.feedback.add_row(
+    name=name,
+    email=email,
+    feedback=feedback,
+    created=datetime.now()
+  )
